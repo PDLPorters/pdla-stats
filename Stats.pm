ffa9a04d15464b339cf62c14df78be48c4b5d2fb
Ed J <mohawk2@users.noreply.github.com>
2016-05-28 16:35:33 +0100

    0.74_1

--- Stats.pm
+++ Stats.pm
@@ -6,7 +6,7 @@ PDLA::Stats - a collection of statistics modules in Perl Data Language, with a q
 
 =cut
 
-$VERSION = '0.74';
+$VERSION = '0.74_1';
 
 $PDLA::onlinedoc->scan(__FILE__) if $PDLA::onlinedoc;
 
